5ab6338e143acb676b553dfc5f27001f2150f489
ManjuSubramaniam <116705401+ManjuSubramaniam@users.noreply.github.com>
2023-01-12 12:02:06 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+      name: "Zara Zara",
+      artist: "Namita Choudhary",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Pyaar Tera 5G",
+      artist: "Chitranshi",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Welcome New Year Hindi",
+      artist: "Shahid Mallya, Akanksha Pathak",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Nobody Is Listening - Better",
+      artist: "Zayn Malik",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Electric",
+      artist: "katy perry",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Fake A Smile",
+      artist: "Alan Walker,Salem Ilese",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
